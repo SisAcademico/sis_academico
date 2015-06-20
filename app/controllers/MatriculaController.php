534f736fc633a2de1f16c965435ef874ad8f9a13
paultbr <paultheo65@gmail.com>
2015-06-20 10:53:36 -0500

    matricula
    matricula listar

---
+++ app/controllers/MatriculaController.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of users
+ *
+ * @author User
+ */
+class MatriculaController extends BaseController{
+
+    /**
+     * LISTAR ASIGNATURAS
+     */
+    public function index()
+    {
+        //
+        $matriculas=Matricula::all();
+        $alumnos=Alumno::all();
+        $auxiliar=DB::table('tmatricula')
+            ->join('talumno', 'tmatricula.idalumno', '=', 'talumno.idalumno')
+            ->select('tmatricula.idmatricula','tmatricula.tipo' ,'talumno.idalumno', 'talumno.nombres','talumno.apellidos','tmatricula.fecha_matricula','tmatricula.idpago')
+            ->get();
+        return View::make('matricula.listar') -> With('matricula',$auxiliar);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    /**
+     * AGREGAR ASIGNATURAS
+     */
+    public function create()
+    {
+        //
+
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+
+    /**
+     * STORE ASIGNATURAS
+     */
+    public function store()
+    {
+
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    /**
+     * ACTUALIZAR LAS ASIGNATURAS
+     */
+    public function edit($id)
+    {
+        //
+
+    }
+
+/**/
+    public function details($id)
+    {
+        
+
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    /**
+     * ACTUALIZAR BD ASIGNATURAS
+     */
+    public function update($id)
+    {
+        //
+
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
+
+?>
---
+++ app/views/matricula/listar.blade.php
@@ -0,0 +1,71 @@
+@extends('_layouts.app')
+@section('titulo')
+    @lang('Matriculas')
+@stop
+@section('titulo_cabecera')
+    @lang('Matriculas')<small>@lang('')</small>
+@stop
+@section('ruta_navegacion')
+    <li><a href="#"><i class="fa fa-list"></i> @lang('sistema.matricula')</a></li>
+    <li class="active">@lang('sistema.listar_matricula')</li>
+@stop
+
+
+@section('contenido')
+    <!-- Main row -->
+    <a href="/sis_academico/public/matricula/create">Crear</a>
+    <div class="row">
+        <!-- INICIO: BOX PANEL -->
+        <div class="col-md-12 col-sm-8">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Listado de Matriculas</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th style="width: 20px">Id</th>
+                            <th >Tipo</th>
+                            <th >Id Alumno</th>
+                            <th >Alumno</th>
+                            <th >Fecha Matricula </th>
+                            <th >Id Pago</th>
+                            <th >Editar</th>
+                            <th >Detalle</th>
+
+                        </tr>
+                        <!-- LISTAR Matriculas-->
+
+                        @foreach($matricula as $matri)
+                            <tr>
+                                <td>{{$matri->idmatricula}}</td>
+                                <td>{{$matri->tipo}} </td>
+                                <td>{{$matri->idalumno}}</td>
+                                <td>{{$matri->nombres}} {{$matri->apellidos}}</td>
+                                <td>{{$matri->fecha_matricula}}</td>
+                                <td>{{$matri->idpago}}</td>
+                                <td>
+                                    <a href="/sis_academico/public/matricula/{{ $matri->idmatricula }}/edit" class="btn btn-xs btn-primary"><i class="fa fa-edit"></i></a>
+                                </td>
+                                <td>
+                                    <a href="/sis_academico/public/matricula/{{ $matri->idmatricula }}/detail" class="btn btn-xs btn-primary"><i class="fa fa-edit"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </table>
+                </div><!-- /.box-body -->
+                <div class="box-footer clearfix text-center">
+                    <ul class="pagination pagination-sm no-margin">
+                        <li><a href="#">&laquo;</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">&raquo;</a></li>
+                    </ul>
+                </div>
+            </div><!-- /.box -->
+        </div>
+        <!-- INICIO: BOX PANEL -->
+    </div><!-- /.box -->
+@endsection
