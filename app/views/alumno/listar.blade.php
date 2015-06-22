98482f7fab35f57608657d379d90bc27a53207f6
BeatsBass <jojhbeatsbass@hotmail.com>
2015-06-22 12:17:36 -0500

    ValidacionCrearAlumno

--- app/views/alumno/listar.blade.php
+++ app/views/alumno/listar.blade.php
@@ -85,7 +85,7 @@
                         <div class="form-group">
                             {{ Form::label('correo', Lang::get('Correo'),array('class'=>'col-sm-2 control-label')) }}
                             <div class="col-sm-10">
-                                <input id="correo" type="text" class="form-control" name="correo"  required>
+                                <input id="correo" type="email" class="form-control" name="correo"  required>
                             </div>
                         </div>
                         <div class="form-group">
@@ -102,7 +102,7 @@
                         </div>
                     </div>
                     <div class="box-footer">
-                        {{ Form::submit(Lang::get('Crear Alumno'), array('class' => 'btn btn-info pull-right')) }}
+                        <input class="btn btn-info pull-right" type="submit" value="Crear Alumno">
                     </div>
                 </div>
                 {{ Form::close() }}
